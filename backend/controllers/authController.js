3dd357c7e65aaca97aeb70eb31f16a6b8b4cfc21
leela-avinash <leelaavinash24@gmail.com>
2025-03-28 13:49:15 +0530

    hostt.....

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -141,7 +141,7 @@ export const logoutUser = (req, res) => {
     res.cookie("jwt", "", {
         httpOnly: true,
         expires: new Date(0),
-        sameSite: "strict",
+        sameSite: "none",
         path: "/", // ensure the path matches where the cookie was set
     });
     res.status(200).json({ success: true, message: "Logged out successfully" });
